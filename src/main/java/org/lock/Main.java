a0cfea5e0256323d474243c8825e95e237e361d2
JJongho123 <dam33534@naver.com>
2024-12-23 14:57:59 +0900

    데드락 발생
    데드락 발생

--- src/main/java/org/lock/Main.java
+++ src/main/java/org/lock/Main.java
@@ -16,7 +16,6 @@ public class Main implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
         String url = "jdbc:mysql://localhost:3306/test2";
         String user = "root";
         String password = "";
@@ -27,66 +26,37 @@ public class Main implements CommandLineRunner {
             conn1.setAutoCommit(false);
             conn2.setAutoCommit(false);
 
-
-            Thread t1 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try (Statement stmt1 = conn1.createStatement()) {
-                        stmt1.executeQuery("SELECT money FROM account WHERE id = 1 FOR UPDATE");
-                        System.out.println("트랜잭션1 lock");
-
-                        Thread.sleep(1000);
-
-                        System.out.println("Transaction 1 trying to lock id = 2...");
-                        stmt1.executeQuery("SELECT money FROM account WHERE id = 2 FOR UPDATE");
-
-                        conn1.commit();
-                        System.out.println("Transaction 1 committed");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            Thread t1 = new Thread(() -> {
+                try (Statement stmt1 = conn1.createStatement()) {
+                    stmt1.executeQuery("SELECT money FROM account WHERE id = 1 FOR UPDATE");
+                    Thread.sleep(1000);
+                    stmt1.executeQuery("SELECT money FROM account WHERE id = 2 FOR UPDATE");
+                    conn1.commit();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             });
 
-            Thread t2 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try (Statement stmt2 = conn2.createStatement()) {
-                        stmt2.executeQuery("SELECT money FROM account WHERE id = 2 FOR UPDATE");
-                        System.out.println("Transaction 2 locked id = 2");
-
-                        Thread.sleep(1000);
-
-                        System.out.println("Transaction 2 trying to lock id = 1...");
-                        stmt2.executeQuery("SELECT money FROM account WHERE id = 1 FOR UPDATE");
-
-                        conn2.commit();
-                        System.out.println("Transaction 2 committed");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            Thread t2 = new Thread(() -> {
+                try (Statement stmt2 = conn2.createStatement()) {
+                    stmt2.executeQuery("SELECT money FROM account WHERE id = 2 FOR UPDATE");
+                    Thread.sleep(1000);
+                    stmt2.executeQuery("SELECT money FROM account WHERE id = 1 FOR UPDATE");
+                    conn2.commit();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             });
 
-            t1.start();
+//            t1.start();
             t2.start();
 
-            t1.join();
-            t2.join();
-
-            ////////////
-
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                }
-            });
-
-            thread.start();
+//             t1.join();
+             t2.join();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
